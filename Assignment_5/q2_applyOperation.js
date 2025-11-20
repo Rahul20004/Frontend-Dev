cbf6e5f87290701df05ba3877d10420d6828c675
Rahul Gupta <156163350+Rahul20004@users.noreply.github.com>
2025-11-20 15:11:27 +0530

    Create q2_applyOperation.js

---
+++ Assignment_5/q2_applyOperation.js
@@ -0,0 +1,15 @@
+// q2_applyOperation.js
+"use strict";
+
+// Higher-order function
+function applyOperation(numbers, operation) {
+    return numbers.map(operation);
+}
+
+const nums = [1, 2, 3, 4];
+
+const doubled = applyOperation(nums, n => n * 2);
+const squared = applyOperation(nums, n => n * n);
+
+console.log("Doubled:", doubled);
+console.log("Squared:", squared);
