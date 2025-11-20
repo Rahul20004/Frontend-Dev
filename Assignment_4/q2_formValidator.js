09989b3fbe0832546f97a5a40f57807d163c04ca
Rahul Gupta <156163350+Rahul20004@users.noreply.github.com>
2025-11-20 14:59:34 +0530

    Create q2_formValidator.js

---
+++ Assignment_4/q2_formValidator.js
@@ -0,0 +1,16 @@
+<form id="studentForm">
+  <input type="text" id="name" placeholder="Name"><br>
+  <span id="nameError"></span><br>
+
+  <input type="text" id="email" placeholder="Email"><br>
+  <span id="emailError"></span><br>
+
+  <input type="text" id="phone" placeholder="Phone"><br>
+  <span id="phoneError"></span><br>
+
+  <input type="password" id="password" placeholder="Password"><br>
+  <span id="passError"></span><br>
+
+  <button type="submit">Register</button>
+</form>
+<script src="q2_formValidator.js"></script>
